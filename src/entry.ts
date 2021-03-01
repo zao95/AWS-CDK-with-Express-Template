19b802b3e838a510aeaf5578395750af5aed7205
zao95 <zao95@naver.com>
2021-03-02 00:11:32 +0900

    ENH local test script

--- src/entry.ts
+++ src/entry.ts
@@ -3,6 +3,7 @@ import { createServer, proxy } from 'aws-serverless-express'
 import express from 'express'
 
 const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
     res.json({
@@ -17,6 +18,10 @@ app.get('/about', (req, res) => {
 })
 
 const server = createServer(app)
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
 
 export const handler: APIGatewayProxyHandler = (event, context) => {
     proxy(server, event, context)
